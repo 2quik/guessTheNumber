a176b64ff20926a137387f359e8589d1e53ab812
2quik <freddybausley4@gmail.com>
2019-02-26 17:15:05 -0800

    Add files via upload
    I learned that you can use the srand function plus putting "NULL" in the time function in the srand function to initialize a random number each time a 'game' is started. Also to get a range of numbers you can use the modulus and add 1 to not get zero as a number.

---
+++ guessTheNumber.cpp
@@ -0,0 +1,50 @@
+// guessTheNumber.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{	// initialize the seed
+	srand((time(NULL)));
+
+	int secretNumber = rand() % 10 + 1;
+	int guess;
+	do {
+		cout << ("Guess the number (1 to 10): ") << endl;
+		cin >> guess;
+		if (guess > secretNumber)
+		{
+			cout << ("The secret number is lower ") << endl;
+		}
+		else if (guess < secretNumber)
+		{
+			cout << "The secret number is higher ";
+		}
+
+		else if (guess == secretNumber) {
+
+			cout << " congratualations !!! You found the number ";
+			
+		}
+	} while (guess != secretNumber);
+	
+	
+
+
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file\
+
